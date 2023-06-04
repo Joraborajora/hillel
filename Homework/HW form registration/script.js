8c9524d93b96274df7e3e6ad5ab8d481501ea764
“Vitali <georgiy.black@gmail.com>
2023-06-04 16:20:31 +0300

    add page profile user and add google API maps

--- Homework/HW form registration/script.js
+++ Homework/HW form registration/script.js
@@ -10,3 +10,47 @@ function redirectToProfile() {
         window.location.href = "my_profile_page.html";
     }, 2000);
 }
+
+function goBack() {
+    window.location.href = "login_page.html";
+}
+function getScreenSize() {
+    console.log(`Screen size: ${window.innerWidth} + ${window.innerHeight}`);
+}
+
+
+// для отримання поточної локації користувача:
+
+function getAddress() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+      } else {
+        console.log("Geolocation is not supported by this browser.");
+      }
+}
+
+// якщо локацію користувача вдалося отримати:
+
+function successCallback(position) {
+    var geocoder = new google.maps.Geocoder();
+    var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    
+    geocoder.geocode({ 'latLng': latlng }, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        if (results[0]) {
+          console.log("Address: " + results[0].formatted_address);
+        } else {
+          console.log("No results found.");
+        }
+      } else {
+        console.log("Geocoder failed due to: " + status);
+      }
+    });
+  }
+
+//   якщо отримати локацію користувача не вдалося або користувач 
+//    відмовився надати доступ до своєї локації:
+
+function errorCallback(error) {
+    console.log("Error getting geolocation: " + error.message);
+  }
