3c06bf03cf5f4cab5e2b314c31d03d4a2720088b
“Vitali <georgiy.black@gmail.com>
2023-08-10 16:29:59 +0300

    create basic react

---
+++ basic-react/src/components/card/index.js
@@ -0,0 +1,14 @@
+import './style.css'
+
+const Card = ()=> {
+    return (
+        <div className = "card">
+            <h4>
+                Card
+            </h4>
+
+        </div>
+    )
+}
+
+export default Card;
---
+++ basic-react/src/components/inpit/index.js
@@ -0,0 +1,12 @@
+import './style.css'
+
+const Input = ()=> {
+    return (
+        <label className="first">
+            <p>Text label </p>
+            <input type="text"/>
+        </label>
+    )
+}
+
+export default Input;
